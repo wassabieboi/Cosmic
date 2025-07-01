f8f67e61fad3346d74a4b68282e1c1a6a04a6315
P0nk <ponkcode@gmail.com>
2025-07-01 07:08:29 +0200

    Add marriage changeset

---
+++ src/main/resources/db/015-marriage.sql
@@ -0,0 +1,17 @@
+CREATE TABLE lb_marriages
+(
+    marriageid INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    husbandid  INT UNSIGNED NOT NULL DEFAULT '0',
+    wifeid     INT UNSIGNED NOT NULL DEFAULT '0',
+    PRIMARY KEY (marriageid)
+);
+
+CREATE TABLE lb_rings
+(
+    id            INT          NOT NULL AUTO_INCREMENT,
+    partnerRingId INT          NOT NULL DEFAULT '0',
+    partnerChrId  INT          NOT NULL DEFAULT '0',
+    itemid        INT          NOT NULL DEFAULT '0',
+    partnername   VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id) USING BTREE
+);
