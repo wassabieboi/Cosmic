0921396c574b2c7d2f32966926d7321c8cd1c342
P0nk <ponkcode@gmail.com>
2025-07-01 12:51:06 +0200

    Integrate quest id adjustments to reactor drop data

--- database/sql/2-db_drops.sql
+++ database/sql/2-db_drops.sql
@@ -21884,20 +21884,6 @@ DELETE FROM temp_data WHERE dropperid >= 9300315 AND dropperid <= 9300324;
  INSERT IGNORE INTO drop_data (`dropperid`, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance`)
  SELECT 3230301, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance` FROM drop_data WHERE dropperid = 3230300;
 
-  # update quest reactor items
-  UPDATE reactordrops SET questid=2086 WHERE itemid=4031165;
-  UPDATE reactordrops SET questid=3407 WHERE itemid=4031141;
-  UPDATE reactordrops SET questid=3407 WHERE itemid=4031142;
-  UPDATE reactordrops SET questid=3407 WHERE itemid=4031143;
-  UPDATE reactordrops SET questid=2067 WHERE itemid=4031150;
-  UPDATE reactordrops SET questid=3239 WHERE itemid=4031092;
-  UPDATE reactordrops SET questid=6002 WHERE itemid=4031508;
-  UPDATE reactordrops SET questid=9351 WHERE itemid=4031258;
-  UPDATE reactordrops SET questid=3083 WHERE itemid >= 4031274 AND itemid <= 4031278;
-  UPDATE reactordrops SET chance=5 WHERE itemid >= 4031274 AND itemid <= 4031278;
-  UPDATE reactordrops SET questid=1008 WHERE itemid=4031161;
-  UPDATE reactordrops SET questid=1008 WHERE itemid=4031162;
-
   # add Amoria Wedding reward boxes
   INSERT INTO `reactordrops` (`reactorid`, `itemid`, `chance`, `questid`) VALUES
 (6802000, 4031423, 5, -1),
