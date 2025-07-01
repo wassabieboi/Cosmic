da17490dbc78e88816a8a61146ee68ef800db525
P0nk <ponkcode@gmail.com>
2025-07-01 09:06:42 +0200

    Add shop data changesets

--- src/main/resources/db/011-shop.sql
+++ src/main/resources/db/011-shop.sql
@@ -4,3 +4,14 @@ CREATE TABLE lb_shops
     npcid  INT          NOT NULL DEFAULT '0',
     PRIMARY KEY (shopid)
 );
+
+CREATE TABLE lb_shopitems
+(
+    shopitemid INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    shopid     INT UNSIGNED NOT NULL,
+    itemid     INT          NOT NULL,
+    price      INT          NOT NULL,
+    pitch      INT          NOT NULL DEFAULT '0',
+    position   INT          NOT NULL COMMENT 'sort is an arbitrary field designed to give leeway when modifying shops. The lowest number is 104 and it increments by 4 for each item to allow decent space for swapping/inserting/removing items.',
+    PRIMARY KEY (shopitemid)
+)
