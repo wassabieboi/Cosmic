150754a74b529837f1254475a98bdf517169ec1b
P0nk <ponkcode@gmail.com>
2025-07-01 16:41:15 +0200

    Add global drop data changeset

--- database/sql/1-db_database.sql
+++ database/sql/1-db_database.sql
@@ -12796,14 +12796,6 @@ CREATE TABLE IF NOT EXISTS `drop_data_global` (
   KEY `mobid` (`continent`) USING BTREE
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 ROW_FORMAT=DYNAMIC AUTO_INCREMENT=5 ;
 
-INSERT INTO `drop_data_global` (`id`, `continent`, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance`, `comments`) VALUES
-(1, -1, 4031865, 1, 1, 0, 35000, 'NX Card 100 PTS'),
-(2, -1, 4031866, 1, 1, 0, 20000, 'NX Card 250 PTS'),
-(3, -1, 4001126, 1, 2, 0, 8000, 'Maple Leaves'),
-(4, -1, 2049100, 1, 1, 0, 1200, 'Chaos Scroll 60%'),
-(5, -1, 2340000, 1, 1, 0, 1200, 'White Scroll'),
-(6, -1, 4001006, 1, 1, 0, 10000, 'Flaming Feather');
-
 CREATE TABLE IF NOT EXISTS `dueyitems` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `PackageId` int(10) unsigned NOT NULL DEFAULT '0',
