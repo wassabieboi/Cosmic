111c4f2677aa6792a42d330c529d5bf07b6be470
P0nk <ponkcode@gmail.com>
2025-07-01 16:22:31 +0200

    Apply subsequent removals of reactor drop data

--- database/sql/2-db_drops.sql
+++ database/sql/2-db_drops.sql
@@ -21930,45 +21930,6 @@ DELETE FROM temp_data WHERE dropperid >= 9300315 AND dropperid <= 9300324;
   DELETE FROM `drop_data` WHERE itemid=8143000;
   DELETE FROM `drop_data` WHERE itemid=2094101;
 
-  # delete all unused content on reactor drop data
-  DELETE FROM `reactordrops` WHERE itemid=1102260;
-  DELETE FROM `reactordrops` WHERE itemid=1342019;
-  DELETE FROM `reactordrops` WHERE itemid=1342020;
-  DELETE FROM `reactordrops` WHERE itemid=1532022;
-  DELETE FROM `reactordrops` WHERE itemid=1532023;
-  DELETE FROM `reactordrops` WHERE itemid=2022712;
-  DELETE FROM `reactordrops` WHERE itemid=3010126;
-  DELETE FROM `reactordrops` WHERE itemid=3012000;
-  DELETE FROM `reactordrops` WHERE itemid=4010008;
-  DELETE FROM `reactordrops` WHERE itemid=4010010;
-  DELETE FROM `reactordrops` WHERE itemid=4022000;
-  DELETE FROM `reactordrops` WHERE itemid=4022001;
-  DELETE FROM `reactordrops` WHERE itemid=4022002;
-  DELETE FROM `reactordrops` WHERE itemid=4022003;
-  DELETE FROM `reactordrops` WHERE itemid=4022004;
-  DELETE FROM `reactordrops` WHERE itemid=4022005;
-  DELETE FROM `reactordrops` WHERE itemid=4022006;
-  DELETE FROM `reactordrops` WHERE itemid=4022007;
-  DELETE FROM `reactordrops` WHERE itemid=4022008;
-  DELETE FROM `reactordrops` WHERE itemid=4022009;
-  DELETE FROM `reactordrops` WHERE itemid=4022010;
-  DELETE FROM `reactordrops` WHERE itemid=4022011;
-  DELETE FROM `reactordrops` WHERE itemid=4022012;
-  DELETE FROM `reactordrops` WHERE itemid=4022013;
-  DELETE FROM `reactordrops` WHERE itemid=4022014;
-  DELETE FROM `reactordrops` WHERE itemid=4022015;
-  DELETE FROM `reactordrops` WHERE itemid=4022016;
-  DELETE FROM `reactordrops` WHERE itemid=4022017;
-  DELETE FROM `reactordrops` WHERE itemid=4022018;
-  DELETE FROM `reactordrops` WHERE itemid=4022019;
-  DELETE FROM `reactordrops` WHERE itemid=4022020;
-  DELETE FROM `reactordrops` WHERE itemid=4022021;
-  DELETE FROM `reactordrops` WHERE itemid=4022022;
-  DELETE FROM `reactordrops` WHERE itemid=4022023;
-  DELETE FROM `reactordrops` WHERE itemid=4032362;
-  DELETE FROM `reactordrops` WHERE itemid=4032363;
-  DELETE FROM `reactordrops` WHERE itemid=4032980;
-
   # MapleMesoFetcher ftw! Set meso drop for remaining mobs which drops more than 4 items.
   INSERT IGNORE INTO drop_data (`dropperid`, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance`) VALUES
 (100122, 0, 35, 52, 0, 400000),
