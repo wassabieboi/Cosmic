11ed85abe000949bc4515353d39e97c058a1c63e
P0nk <ponkcode@gmail.com>
2025-07-03 21:30:46 +0200

    Copy Dark Yeti drops to Transformed Dark Yeti

--- database/sql/2-db_drops.sql
+++ database/sql/2-db_drops.sql
@@ -28,10 +28,6 @@ USE `cosmic`;
  -- Copying drops from some mobs to other versions of them
 
  INSERT IGNORE INTO drop_data (`dropperid`, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance`)
- SELECT 6400001, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance`
- FROM drop_data WHERE dropperid = 6400000;
-
- INSERT IGNORE INTO drop_data (`dropperid`, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance`)
  SELECT 6130102, `itemid`, `minimum_quantity`, `maximum_quantity`, `questid`, `chance`
  FROM drop_data WHERE dropperid = 6130103;
 
